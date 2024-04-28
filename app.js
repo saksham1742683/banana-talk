60f120221bc6fc430b1de3ba4ef95e87632bb987
saksham1742683 <154737227+saksham1742683@users.noreply.github.com>
2024-04-28 12:09:36 +0530

    API  integrated successfully

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const btn= document.querySelector('#click-btn')
 const textinput= document.querySelector('#text-input')
 const outputDiv=document.querySelector('#output')
 
-
 function newFunction() {
-  outputDiv.innerText =  "123jdkfndskfskfj"+ textinput.value ;
+    const url=`https://api.funtranslations.com/translate/minion.json?text=${textinput.value}`
+
+    
+    fetch(url).then(response=>response.json()).then(json=>
+    
+        outputDiv.innerText = json.contents.translated)
 }; 
 
 
 btn.addEventListener("click", newFunction);
+
+
